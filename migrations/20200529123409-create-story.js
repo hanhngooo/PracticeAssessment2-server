e646735805d9b7916f1461460ba4b478d262ab20
hanhngooo <hanhngo1795@gmail.com>
2020-05-29 15:41:32 +0200

    Add relations of user-homepage-story in models & migration

---
+++ migrations/20200529123409-create-story.js
@@ -0,0 +1,43 @@
+"use strict";
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("stories", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      content: {
+        type: Sequelize.TEXT,
+      },
+      imageUrl: {
+        type: Sequelize.STRING,
+      },
+      homepageId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "homepages",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "SET NULL",
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("stories");
+  },
+};
