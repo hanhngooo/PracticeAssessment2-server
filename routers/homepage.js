21bd8921fb4c230394a8992d20d91dbee196aa82
hanhngooo <hanhngo1795@gmail.com>
2020-06-03 00:01:17 +0200

    working on posting story

--- routers/homepage.js
+++ routers/homepage.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const Homepage = require("../models").homepage;
 const router = new Router();
 const Story = require("../models").story;
+const authMiddleware = require("../auth/middleware");
 
 router.get("/", async (req, res) => {
   const limit = req.query.limit || 10;
@@ -16,7 +17,7 @@ router.get("/", async (req, res) => {
 });
 router.get("/:id", async (req, res) => {
   const { id } = req.params;
-  console.log("id", id);
+  // console.log("id", id);
 
   const homepageById = await Homepage.findByPk(id, {
     include: [Story],
@@ -26,5 +27,23 @@ router.get("/:id", async (req, res) => {
     return res.status(404).send({ message: "Homepage not found" });
   }
   res.status(200).send(homepageById);
+});
+router.post("/:id/newStory", authMiddleware, async (req, res) => {
+  const { id } = req.params;
+  const homepageById = await Homepage.findByPk(id);
+
+  const { name, content, imageUrl } = req.body;
+  if (!name || !content || !imageUrl) {
+    res
+      .status(400)
+      .send("Please provide name, content and image of your story");
+  }
+  try {
+    const newStory = await Story.create({
+      name: name,
+      content: content,
+      imageUrl: imageUrl,
+    });
+  } catch (error) {}
 });
 module.exports = router;
